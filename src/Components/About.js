ecc015d37d407e40621377fc082a5f3c8465a8d1
Jon Christie <fullstack11235@gmail.com>
2020-09-26 14:27:09 -0400

    m

--- src/Components/About.js
+++ src/Components/About.js
@@ -38,25 +38,8 @@ class About extends Component {
           </div>
           <div className="seven columns">
             <h2>About Me</h2>
-            <p>
-                   Since 2001 when I moved into Morrison Dorm on Carolina's south
-                   campus, just a few miles away from my current home, I have been
-                   either studying numbers, teaching numbers, or using numbers to
-                   create music. Having indulged myself so deeply in the wonders of
-                   mathematics and music begs the question, "Why did I not become a 
-                   web developer 20 years ago?" Well hind sight is 20-20, right? Or 
-              is it? <br /><br />Either way, I've gathered quite an array of usefull skills
-                   to bring to the web industry, and whatever
-              led me to the room I'm in today has provided me, and your company, with a
-              fresh mind, eager for problems to solve, time to save, lives to enrich, and money to make. In
-              addition to my coding-related experiences I have been keeping the
-              actual practice and implementation of coding languages and design close by my
-              side as both a hobby and more importanatly, a way to increase productivity and
-              quality of work.<br/> <br/>Thank you for stopping by and seeing what I
-              have to offer. I hope my portfolio invokes questions and
-              spawns enough curiosity to hear back from you. Feel free to use my contact
-              information under image in this section or a social media link to connect found at the top 
-               and bottom of the site. Let's connect!
+            <p>Since 2001 when I moved into Morrison Dorm on Carolina's south
+ campus, just a few miles away from my current home, I have been either studying numbers, teaching numbers, or using numbers to create music. Having indulged myself so deeply in the wonders of mathematics and music begs the question, "Why did I not become a web developer 20 years ago?" Well hindsight is 20-20, right? Or is it?<br/><br/>Either way, I've gathered quite an array of useful skills to bring to the web industry, and whatever led me to the room I'm in today has provided me, and your company, with a fresh mind, eager for problems to solve, time to save, lives to enrich, and money to make. In addition to my coding-related experiences, I have been keeping the actual practice and implementation of coding languages and design close by my side as both a hobby and more importantly, a way to increase productivity and quality of work.<br/> <br/>Thank you for stopping by and seeing what I have to offer. I hope my portfolio invokes questions and spawns enough curiosity to hear back from you. Feel free to use my contact information under the image in this section or a social media link to connect found at the top and bottom of the site. Let's connect!
             </p>
             <div className="download">
               <p>
