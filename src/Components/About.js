c9000b2a08fd48cd83770f1222b544931a939fca
Jon Christie <fullstack11235@gmail.com>
2021-05-04 18:26:55 -0400

    resume update

--- src/Components/About.js
+++ src/Components/About.js
@@ -9,7 +9,7 @@ class About extends Component {
       var zip = this.props.data.address.zip;
       var phone = this.props.data.phone;
       var email = this.props.data.email;
-      var resumeDownload = this.props.data.resumedownload;
+      var resumeDownload = this.props.data.resumeDownload;
     }
     return (
       <section id="about">
